73d19ad23b2b1f0c678109f187970578fbb13e00
kiennpt <nguyenphuctrungkien@gmail.com>
2021-08-13 00:03:46 +0700

    add current rank

--- src/index.tsx
+++ src/index.tsx
@@ -6,34 +6,32 @@ import RootLiveGameModule from './modules/livegame/src';
 import { makeStore } from './redux/store';
 const store = makeStore();
 
-// ReactDOM.render(
-//     <Router basename="/livegame">
-//         <Provider store={store.store}>
-//             <PersistGate loading={null} persistor={store.persistor}>
-//                 <RootLiveGameModule
-//                     oldSystemUser={{
-//                         account: "kientest2",
-//                         name: "test 2"
-//                     }}
-//                 />
-//             </PersistGate>
-//         </Provider>
-//     </Router>
-//     ,
-//     document.getElementById("livegame"));
+ReactDOM.render(
+    <Provider store={store.store}>
+        <PersistGate loading={null} persistor={store.persistor}>
+            <RootLiveGameModule
+                oldSystemUser={{
+                    account: "kientest6",
+                    name: "test 6"
+                }}
+            />
+        </PersistGate>
+    </Provider>
+    ,
+    document.getElementById("livegame"));
 
 
 export const initDeothetinduoc = (props: any) => {
     console.log("PROPS TRUYỀN SANG ĐÂY LÀY: ", props);
     ReactDOM.render(
         // <Router basename="/livegame">
-            <Provider store={store.store}>
-                <PersistGate loading={null} persistor={store.persistor}>
-                    <RootLiveGameModule
-                        oldSystemUser={props}
-                    />
-                </PersistGate>
-            </Provider>
+        <Provider store={store.store}>
+            <PersistGate loading={null} persistor={store.persistor}>
+                <RootLiveGameModule
+                    oldSystemUser={props}
+                />
+            </PersistGate>
+        </Provider>
         // </Router>
         ,
         document.getElementById("livegame"));
