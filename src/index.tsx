5abc03218cbdf0ec0fb913b3daba5473985eb520
kiennpt <nguyenphuctrungkien@gmail.com>
2021-08-12 09:41:25 +0700

    fix router

--- src/index.tsx
+++ src/index.tsx
@@ -4,25 +4,41 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import RootLiveGameModule from './modules/livegame/src';
 import { makeStore } from './redux/store';
+import {
+    BrowserRouter as Router, useHistory
+} from "react-router-dom";
 const store = makeStore();
 
 ReactDOM.render(
-    <Provider store={store.store}>
-        <PersistGate loading={null} persistor={store.persistor}>
-            <RootLiveGameModule 
-                oldSystemUser={{
-                    account: "kien1712111",
-                    name: "hàng cũ nhưng đẹp trai"
-                }}
-            />
-        </PersistGate>
-    </Provider>
+    <Router basename="/livegame">
+        <Provider store={store.store}>
+            <PersistGate loading={null} persistor={store.persistor}>
+                <RootLiveGameModule
+                    oldSystemUser={{
+                        account: "kien1712111",
+                        name: "hàng cũ nhưng đẹp trai"
+                    }}
+                />
+            </PersistGate>
+        </Provider>
+    </Router>
     ,
     document.getElementById("livegame"));
-// export const initDeothetinduoc = (props: any) => {
-//     console.log("ADSFASDFASDFASDFAS", props);
 
-//     ReactDOM.render(
-//         <App data={props} />,
-//         document.getElementById("livegame"));
-// }
+
+export const initDeothetinduoc = (props: any) => {
+    console.log("PROPS TRUYỀN SANG ĐÂY LÀY: ", props);
+
+    ReactDOM.render(
+        <Router basename="/livegame">
+            <Provider store={store.store}>
+                <PersistGate loading={null} persistor={store.persistor}>
+                    <RootLiveGameModule
+                        oldSystemUser={props}
+                    />
+                </PersistGate>
+            </Provider>
+        </Router>
+        ,
+        document.getElementById("livegame"));
+}
